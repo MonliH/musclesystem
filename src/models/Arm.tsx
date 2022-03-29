331fd33cc30ed42b1f31a8ad405cb69dee318d7b
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2022-03-28 22:01:55 -0400

    Add constraint

--- src/models/Arm.tsx
+++ src/models/Arm.tsx
@@ -1,60 +1,74 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import { Box, useGLTF } from "@react-three/drei";
-import Model from "models/Model";
-import { humerousOffset } from "models/Humerous";
-import { BodyType, ShapeType, useRigidBody } from "@glavin001/use-ammojs";
+import {
+  useBox,
+  useHingeConstraint,
+  usePlane,
+  useSphere,
+  useSpring,
+} from "@react-three/cannon";
 
 export default function Arm({ ...props }) {
   const { nodes, materials } = useGLTF("/arm.glb");
-  const [boxRef] = useRigidBody(() => ({
-    bodyType: BodyType.STATIC,
-    shapeType: ShapeType.BOX,
-    // position: [0, -5, 0],
-    // halfExtents: [10, 0.5, 10],
+  const w = 3.35;
+  const [humerous] = useBox(() => ({
+    mass: 1,
+    args: [w, 0.5, 0.5],
+    rotation: [0, Math.PI / 2, -0.25],
+    position: [0, 0.35, 1.45],
+    // type: "Static",
   }));
+  useBox(() => ({ position: [0, -0.6, -0.7], args: [2.5, 0.5, 2.5] }));
+  useBox(() => ({ position: [0, -1, -3], args: [2.5, 2.5, 2.5] }));
+  const s = 0.25;
+  const [joint] = useBox(() => ({
+    args: [s, s, s],
+    rotation: [0, Math.PI / 2, 0],
+    position: [0, 0, 0],
+    mass: 0,
+    type: "Static",
+  }));
+  useHingeConstraint(joint, humerous, {
+    axisA: [0, 0, 1],
+    axisB: [0, 0, 1],
+    collideConnected: false,
+    pivotA: [0, 0, 0],
+    pivotB: [w * 0.5 - 0.17, 0.02, 0],
+  });
+  const [attachment] = useBox(() => ({
+    args: [s, s, s],
+    position: [0, 0.25, -3.5],
+    mass: 0,
+    type: "Static",
+  }));
+  useSpring(attachment, humerous, {
+    localAnchorB: [w * 0.5 - 0.3, 0.05, 0],
+    stiffness: 1000,
+  });
   return (
     <>
-      <Model
-        path={"/humerous2.glb"}
-        prefix={"Humerus_Humerus001_hull_"}
-        nSegments={81}
-        offsets={humerousOffset}
-      />
-
-      {/* <Model
-        path={"/radius.glb"}
-        prefix={"Radius_Radius001_hull_"}
-        nSegments={42}
-      />
-<Model
-        path={"/ulna.glb"}
-        prefix={"Ulna_Ulna001_hull_"}
-        nSegments={71}
-      />
-      */}
-      <group position={[0, 1, 0]}>
-        <mesh
-          geometry={nodes.Humerus_Humerus001.geometry}
-          rotation={[Math.PI / 2, 0, 0]}
-        >
-          <meshPhysicalMaterial />
-        </mesh>
+      <mesh
+        geometry={nodes.Humerus_Humerus001.geometry}
+        rotation={[Math.PI / 2, 0, 0]}
+      >
+        <meshPhysicalMaterial />
+      </mesh>
+      <group ref={humerous}>
         <mesh
           geometry={nodes.Radius_Radius001.geometry}
-          rotation={[Math.PI / 2, 0, 0]}
+          rotation={[Math.PI / 2, 0.25, Math.PI / 2]}
+          position={[1.5, 0, 0]}
         >
           <meshPhysicalMaterial />
         </mesh>
         <mesh
           geometry={nodes.Ulna_Ulna001.geometry}
-          rotation={[Math.PI / 2, 0, 0]}
+          rotation={[Math.PI / 2, 0.25, Math.PI / 2]}
+          position={[1.5, 0, 0]}
         >
           <meshPhysicalMaterial />
         </mesh>
       </group>
-      <Box args={[20, 5, 20]} receiveShadow position={[0, -5, 0]} ref={boxRef}>
-        <meshPhysicalMaterial attach="material" color="grey" />
-      </Box>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,25 @@
 import { Box, Text } from "@chakra-ui/react";
-import { Physics } from "@glavin001/use-ammojs";
+import { Debug, Physics, usePlane } from "@react-three/cannon";
 import { OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import type { NextPage } from "next";
 import Arm from "models/Arm";
 import { Suspense } from "react";
 
-function ArmSim() {
-  return <Arm />;
-}
-
 const Home: NextPage = () => {
   return (
     <Box w="100%" h="100%">
-      <Canvas
-        shadows
-        gl={{ alpha: true }}
-        camera={{ position: [-1, 5, 5], fov: 45 }}
-      >
+      <Canvas shadows camera={{ position: [-1, 5, 5], fov: 45 }}>
         <ambientLight />
         <pointLight position={[1, 1, 1]} castShadow />
         <pointLight position={[-1, -1, -1]} castShadow />
         <OrbitControls />
+        <axesHelper />
         <Suspense fallback={<Text>Loading...</Text>}>
-          <Physics drawDebug>
-            <ArmSim />
+          <Physics allowSleep>
+            {/* <Debug scale={1.01}> */}
+            <Arm />
+            {/* </Debug> */}
           </Physics>
         </Suspense>
       </Canvas>
